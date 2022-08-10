a6f04c836c2efc456c8119b7d9d2cb8b0c754c35
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-10 17:49:04 +0100

    add the add, remove and change todo value functions

--- src/index.js
+++ src/index.js
@@ -1,48 +1,33 @@
 import './styles.css';
 
 const list = document.querySelector('ul');
-const form = document.querySelector('form')
-
+const form = document.querySelector('form');
 let todos = [];
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  let newTodo = {
-    description : form.tasks.value,
-    completed : false,
-    index : todos.length + 1
-  }
-  form.tasks.value = ''
-  todos.push(newTodo)
-  localStorage.setItem('todos', JSON.stringify(todos))
-  display()
-})
-
-let display = () => {
-  list.innerHTML = ''
-  todos.map((todo, i) => {
-  console.log(todos)
+const display = () => {
+  list.innerHTML = '';
+  todos.forEach((todo) => {
     const li = document.createElement('li');
     const checked = document.createElement('input');
     const text = document.createElement('input');
     const menuIcon = document.createElement('span');
     const deleteIcon = document.createElement('span');
     li.setAttribute('class', 'li-wrapper');
-    checked.type = 'checkbox'
-    checked.name = 'task-done'
-    checked.checked = false
-    checked.id = todo.index
-    text.type = 'text'
-    text.name = 'task'
+    checked.type = 'checkbox';
+    checked.name = 'task-done';
+    checked.checked = false;
+    checked.id = todo.index;
+    text.type = 'text';
+    text.name = 'task';
     text.value = todo.description;
-    text.id = todo.index
-    text.disabled= true
-    text.style.cursor = 'pointer'
-    text.style.background = 'none'
-    text.style.color= 'black'
+    text.id = todo.index;
+    text.disabled = true;
+    text.style.cursor = 'pointer';
+    text.style.background = 'none';
+    text.style.color = 'black';
     menuIcon.innerHTML = '<i class="fa-solid fa-ellipsis-vertical" for="select"></i>';
     deleteIcon.innerHTML = `<i class="fa-solid fa-trash" id="${todo.index}"></i>`;
-    deleteIcon.style.display = 'none'
+    deleteIcon.style.display = 'none';
     li.appendChild(checked);
     li.appendChild(text);
     li.appendChild(menuIcon);
@@ -50,55 +35,75 @@ let display = () => {
     list.appendChild(li);
 
     menuIcon.addEventListener('click', () => {
-      deleteIcon.style.display = 'block'
-      menuIcon.style.display = 'none'
-      text.disabled = false
-      text.style.background = ''
+      deleteIcon.style.display = 'block';
+      menuIcon.style.display = 'none';
+      text.disabled = false;
+      text.style.background = '';
       text.addEventListener('keydown', (e) => {
+        const textId = e.target.id;
         if (e.key === 'Enter') {
-        console.log(e.target.id)
-        }
-      })
+          todos = todos.map((todo) => {
+            if (Number(textId) === todo.index) {
+              return {
+                description: text.value,
+                completed: false,
+                index: todo.index,
+              };
+            }
 
-    })
+            return {
+              description: todo.description,
+              completed: false,
+              index: todo.index,
+            };
+          });
+          localStorage.setItem('todos', JSON.stringify(todos));
+          display();
+        }
+      });
+    });
 
     deleteIcon.addEventListener('click', (e) => {
-      let taskId = e.target.id
-      console.log(Number(taskId))
-      console.log(todo.index)
-      todos = todos.filter(todo => todo.index !== Number(taskId))
+      const taskId = e.target.id;
+
+      todos = todos.filter((todo) => todo.index !== Number(taskId));
       todos = todos.map((todo) => {
-        if(todo.index > taskId) {
+        if (todo.index > taskId) {
           return {
-            description : todo.description,
-            completed : todo.completed,
-            index: todo.index - 1
-          }
+            description: todo.description,
+            completed: todo.completed,
+            index: todo.index - 1,
+          };
         }
-        else {
-          return {
-            description : todo.description,
-            completed : todo.completed,
-            index: todo.index
-          }
-        }
-       })
-      localStorage.setItem('todos', JSON.stringify(todos))
-      e.target.parentElement.parentElement.remove()
-      console.log(todos)
-    })
 
-    text.addEventListener('keyup', (e) => {
-      if (e.key === 'Enter') {
-        console.log(e.target.value)
+        return {
+          description: todo.description,
+          completed: todo.completed,
+          index: todo.index,
+        };
+      });
+      localStorage.setItem('todos', JSON.stringify(todos));
+      e.target.parentElement.parentElement.remove();
+    });
 
-      }
-    })
   });
 // }
-}
+};
 
-if(localStorage.getItem('todos')) {
-  todos = JSON.parse(localStorage.getItem('todos'))
-  display()
-  }
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newTodo = {
+    description: form.tasks.value,
+    completed: false,
+    index: todos.length + 1,
+  };
+  form.tasks.value = '';
+  todos.push(newTodo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  display();
+});
+
+if (localStorage.getItem('todos')) {
+  todos = JSON.parse(localStorage.getItem('todos'));
+  display();
+}
