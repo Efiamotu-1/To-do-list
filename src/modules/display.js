4c43f8c363cef0fc82d975eb002f5b45a60ef985
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-14 08:48:34 +0100

    add changes but still not working

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,6 +1,8 @@
 import data from './data.js';
 import { list } from './domElements.js';
 
+let dragStartId = 0;
+
 const display = () => {
   list.innerHTML = '';
   data.todos.forEach((todo) => {
@@ -25,6 +27,7 @@ const display = () => {
     menuIcon.innerHTML = '<i class="fa-solid fa-ellipsis-vertical" for="select"></i>';
     deleteIcon.innerHTML = `<i class="fa-solid fa fa-trash" id="${todo.index}"></i>`;
     deleteIcon.style.display = 'none';
+    li.draggable = 'true'
     li.appendChild(checked);
     li.appendChild(text);
     li.appendChild(menuIcon);
@@ -34,6 +37,64 @@ const display = () => {
       checked.checked = true;
       text.style.textDecoration = 'line-through';
     }
+
+       // Drag event
+   li.addEventListener('dragstart', dragStart)
+
+   li.addEventListener('dragend', dragEnd)
+   li.addEventListener('dragover', dragOver)
+   li.addEventListener('dragenter', dragEnter)
+   li.addEventListener('dragleave', dragLeave)
+   li.addEventListener('drop', dragDrop)
+ 
+ 
+   // Drag Functions
+   function dragStart(e) {
+     dragStartId = e.target.id
+     console.log(data.todos)
+     this.classList.add('selected')     
+     setTimeout(() => this.classList.add('invisible'), 0) 
+   }
+ 
+   function dragEnd() {
+     this.classList.remove('invisible')
+     this.classList.remove('selected')     
+   }
+ 
+   function dragOver(e) {
+     e.preventDefault()
+ 
+   }
+ 
+   function dragEnter(e) {
+     e.preventDefault()
+     this.classList.add('hovered')    
+   }
+ 
+   function dragLeave() {
+     this.classList.remove('hovered')     
+   }
+ 
+   function dragDrop(e) {    
+     let count = 1
+     let replaceIndex = e.target.id - 1
+    //  console.log(dragStartId)
+    //  console.log(replaceIndex)
+     let indexNew = dragStartId -1
+     let [replaceValue] = data.todos.splice(indexNew, 1)
+     data.todos.splice(replaceIndex, 0, replaceValue)
+     data.todos = data.todos.map((todo) => {
+       return {
+         description : todo.description,
+         completed : todo.completed,
+         index : count++
+       }
+     })
+     this.classList.remove('hovered')     
+     console.log(data.todos)
+     display()
+   }
+ 
 
     const deleteTodo = (e) => {
       const taskId = e.target.id;
